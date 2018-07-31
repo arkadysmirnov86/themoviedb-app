5809d6ac667130cccfa3794bce8a4b9b0ccf307b
Arkady Smirnov <arkady.smirnov@e-legion.com>
2018-07-31 21:40:24 +0300

    project initialized

---
+++ themoviedb-app/Code/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  themoviedb-app
+//
+//  Created by Arkady Smirnov on 7/31/18.
+//  Copyright © 2018 Arkady Smirnov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+}
+
---
+++ themoviedb-app/Code/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  themoviedb-app
+//
+//  Created by Arkady Smirnov on 7/31/18.
+//  Copyright © 2018 Arkady Smirnov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
