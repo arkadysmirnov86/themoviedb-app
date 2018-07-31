0170a2c667574344a7adc3fee39d88fc8073441d
Arkady Smirnov <arkady.smirnov@e-legion.com>
2018-07-31 22:17:40 +0300

    implemented fetch method

--- themoviedb-app/Code/Services/DataProvider.swift
+++ themoviedb-app/Code/Services/DataProvider.swift
@@ -10,11 +10,50 @@ import Foundation
 
 protocol DataProviderProtocol {
     typealias ResultType = PageEntity<FilmInfoEnity>
-    func fetchFilms(complitionhandler: (Result<ResultType>) -> Void)
+    
+    func fetchFilms(query: String, page: Int, completionhandler: @escaping (Result<ResultType>) -> Void)
 }
 
 class DataProvider: DataProviderProtocol {
-    func fetchFilms(complitionhandler: (Result<ResultType>) -> Void) {
-        fatalError("method not implemeted")
+    typealias ResultType = PageEntity<FilmInfoEnity>
+    
+    enum Errors: Error {
+        case unknownError
+        case invalidParams
+        case dataIsEmpty
     }
+    func fetchFilms(query: String, page: Int, completionhandler: @escaping (Result<ResultType>) -> Void) {
+        let session = URLSession.shared
+        
+        guard let url = URL(string: String(format: .endpointFormat, query, page)) else {
+            completionhandler(Result<ResultType>.error(Errors.invalidParams))
+            return
+        }
+        
+        let task = session.dataTask(with: url) { (data, response, error) in
+            guard error != nil else {
+                let error = error ?? Errors.unknownError
+                completionhandler(Result<ResultType>.error(error))
+                return
+            }
+            
+            guard let data = data else {
+                completionhandler(Result<ResultType>.error(Errors.dataIsEmpty))
+                return
+            }
+            
+            do {
+                let value = try JSONDecoder.init().decode(ResultType.self, from: data)
+                completionhandler(Result<ResultType>.success(value))
+            } catch {
+                completionhandler(Result<ResultType>.error(error))
+            }
+        }
+        
+        task.resume()
+    }
+}
+
+private extension String {
+    static let endpointFormat = "http://api.themoviedb.org/3/search/movie?api_key=2696829a81b1b5827d515ff121700838&query=%@&page=%d"
 }
