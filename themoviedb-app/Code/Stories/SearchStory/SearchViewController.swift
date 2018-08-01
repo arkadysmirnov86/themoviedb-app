34bd5f97f36cbb9100a4efc80106b53a1adeab76
Arkady Smirnov <arkady.smirnov@e-legion.com>
2018-08-01 23:47:39 +0300

    implemented data source

--- themoviedb-app/Code/Stories/SearchStory/SearchViewController.swift
+++ themoviedb-app/Code/Stories/SearchStory/SearchViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class SearchViewController: UIViewController {
     
+    @IBOutlet private weak var searchField: UITextField? {
+        didSet {
+         
+        }
+    }
+    
+    @IBOutlet private weak var tableView: UITableView? {
+        didSet {
+            configureTableView()
+        }
+    }
+    
     var viewModel: SearchViewModel? {
         didSet {
             bindViewModel()
@@ -29,12 +41,30 @@ class SearchViewController: UIViewController {
         
     }
     
-    private func updateUI() {
-        
-    }
-    
     private func showError() {
         let alertController = UIAlertController(title: "error", message: "error message", preferredStyle: UIAlertControllerStyle.alert)
         alertController.addAction(UIAlertAction.init(title: "ok", style: UIAlertActionStyle.default, handler: nil))
     }
+    
+    private func configureTableView() {
+        tableView?.register(UITableViewCell.self, forCellReuseIdentifier: .cellReuseIdentifier)
+        tableView?.dataSource = self
+    }
+}
+
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.viewModel?.history.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: .cellReuseIdentifier) ?? UITableViewCell()
+        cell.imageView?.image = UIImage()
+        cell.textLabel?.text = self.viewModel?.history[indexPath.row]
+        return cell
+    }
+}
+
+private extension String {
+    static let cellReuseIdentifier = "cell"
 }
